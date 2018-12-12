2d60626eb3cdd90dc459637fe0d9651fbf7116e7
zla <zhenglongan@163.com>
2018-12-12 20:29:11 +0800

    备份1：新增IO功能，在文件io_function.c/.h中，整理除了IO逻辑功能表和物理通道表， 同时兼容了六轴SC0720和四轴SC0719版，综合考虑两种情况以及后续可能出现的更多硬件 版型，此后在IO方面不再根据宏定义去选板子了，而是热选择，现在已经完成框架的搭建， 编写了部分函数，这里先备份下
    Signed-off-by: zla <zhenglongan@163.com>

--- project_code/Project/MDKARM/Objects/initial.d
+++ project_code/Project/MDKARM/Objects/initial.d
@@ -1,6 +1,6 @@
 .\objects\initial.o: ..\..\Applications\User\src\initial.c
 .\objects\initial.o: ..\..\Applications\User\inc\common.h
 .\objects\initial.o: ..\..\Applications\User\inc\typedef.h
-.\objects\initial.o: C:\Keil_v5\ARM\ARMCC\include\stdint.h
+.\objects\initial.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\objects\initial.o: ..\..\Applications\System\inc\sys_variable.h
 .\objects\initial.o: ..\..\Applications\User\inc\communication.h
--- project_code/Project/MDKARM/Objects/parameter.d
+++ project_code/Project/MDKARM/Objects/parameter.d
@@ -1,4 +1,4 @@
 .\objects\parameter.o: ..\..\Applications\User\src\parameter.c
 .\objects\parameter.o: ..\..\Applications\User\inc\common.h
 .\objects\parameter.o: ..\..\Applications\User\inc\typedef.h
-.\objects\parameter.o: C:\Keil_v5\ARM\ARMCC\include\stdint.h
+.\objects\parameter.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
