8e2d2667828c2c722774f82919ed1ddb8513bc54
zla <zhenglongan@163.com>
2018-12-25 19:23:03 +0800

    2018_12_24孔工发来的程序，然后换上新的IO映射程序，这里先备份下

--- project_code/Project/MDKARM/Objects/step_motor_drv.d
+++ project_code/Project/MDKARM/Objects/step_motor_drv.d
@@ -1,10 +1,9 @@
 .\objects\step_motor_drv.o: ..\..\Applications\User\src\step_motor_drv.c
-.\objects\step_motor_drv.o: ..\..\Applications\Bsp\inc\bsp_spibus.h
-.\objects\step_motor_drv.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\objects\step_motor_drv.o: ..\..\Applications\Bsp\inc\spi_master.h
 .\objects\step_motor_drv.o: ..\..\Libraries\XMCLib\inc\xmc_gpio.h
 .\objects\step_motor_drv.o: ..\..\Libraries\XMCLib\inc\xmc_common.h
 .\objects\step_motor_drv.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\objects\step_motor_drv.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\objects\step_motor_drv.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
 .\objects\step_motor_drv.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
 .\objects\step_motor_drv.o: ..\..\Libraries\XMCLib\inc\xmc_device.h
@@ -22,6 +21,12 @@
 .\objects\step_motor_drv.o: ..\..\Libraries\XMCLib\inc\xmc4_usic_map.h
 .\objects\step_motor_drv.o: ..\..\Applications\Bsp\inc\spi_master_conf.h
 .\objects\step_motor_drv.o: ..\..\Applications\Bsp\inc\spi_master_extern.h
+.\objects\step_motor_drv.o: ..\..\Applications\Bsp\inc\bsp_spibus.h
+.\objects\step_motor_drv.o: ..\..\Applications\Bsp\inc\bsp_spiflash.h
+.\objects\step_motor_drv.o: ..\..\Libraries\XMCLib\inc\xmc_dma.h
+.\objects\step_motor_drv.o: ..\..\Libraries\XMCLib\inc\xmc_dma_map.h
+.\objects\step_motor_drv.o: ..\..\Applications\App\inc\app_gpio.h
+.\objects\step_motor_drv.o: ..\..\Applications\Bsp\inc\bsp_gpio.h
 .\objects\step_motor_drv.o: ..\..\Applications\User\inc\common.h
 .\objects\step_motor_drv.o: ..\..\Applications\User\inc\typedef.h
 .\objects\step_motor_drv.o: ..\..\Applications\System\inc\sys_variable.h
