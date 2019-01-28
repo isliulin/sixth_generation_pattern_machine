22d31ebbd8a538011180977c058a5514cf0a725c
zla <zhenglongan@163.com>
2019-01-28 17:40:26 +0800

    2019-1-28 和牛泽农联调，完成对步进电机的配置过程，并能够实际动作起来

--- project_code/Project/MDKARM/Objects/io_file_struct.d
+++ project_code/Project/MDKARM/Objects/io_file_struct.d
@@ -1,3 +1,3 @@
 .\objects\io_file_struct.o: ..\..\Applications\User\src\io_file_struct.c
 .\objects\io_file_struct.o: ..\..\Applications\User\inc\io_file_struct.h
-.\objects\io_file_struct.o: C:\Keil_v5\ARM\ARMCC\include\stdint.h
+.\objects\io_file_struct.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- project_code/Project/MDKARM/Objects/unit_lrc.d
+++ project_code/Project/MDKARM/Objects/unit_lrc.d
@@ -1,3 +1,3 @@
 .\objects\unit_lrc.o: ..\..\Applications\Unit\src\unit_lrc.c
 .\objects\unit_lrc.o: ..\..\Applications\Unit\inc\unit_lrc.h
-.\objects\unit_lrc.o: C:\Keil_v5\ARM\ARMCC\include\stdint.h
+.\objects\unit_lrc.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
