199b38de2f7335fd1b23ca496df29e82eb394418
zla <zhenglongan@163.com>
2019-03-01 11:59:27 +0800

    2019-3-1  合并了孔工调试的程序，主要是增加了DA调试程序，使用DA来打出主轴角度波形和指令发送时机， 还有一些其他的修改。  对四轴SC0719版进行了调试，系统参数第1组的参数1-2用于选择是六轴还是四轴，现在都可以支持 了，主要是IO的初始化修改了。
    Signed-off-by: zla <zhenglongan@163.com>

---
+++ project_code/Applications/User/inc/debug.h
@@ -0,0 +1,6 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+extern void debug_mode_init(void);
+
+#endif
--- project_code/Project/MDKARM/Objects/main.d
+++ project_code/Project/MDKARM/Objects/main.d
@@ -77,3 +77,4 @@
 .\objects\main.o: ..\..\Applications\User\inc\io_function.h
 .\objects\main.o: ..\..\Applications\User\inc\action.h
 .\objects\main.o: ..\..\Applications\User\inc\stepper_function.h
+.\objects\main.o: ..\..\Applications\User\inc\debug.h
