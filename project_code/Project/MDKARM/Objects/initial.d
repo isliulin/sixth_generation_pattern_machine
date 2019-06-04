c38cf56b6848b83d68c1604ddb0af1c8068afcbf
zla <zhenglongan@163.com>
2019-06-04 19:31:01 +0800

    2019-6-4 丁工和孔工近期调试的同步模式，目前在六轴SC0720A上可以慢速缝制， 今天在丁工的基础上，修改了下面两点： （1）取消DA1作为主轴角度的调试输出，因为DA1控制了夹线器； （2）按照丁工要求，同步模式的角度范围放到了主控保留参数2上，即第2组的73-74和 173-174来分别设置X和Y的同步模式动框角度范围
    注意：目前使用的是SC0720A新六轴板件，IO情况和SC0719更为接近，后续需要再
    重新整理IO管理部分。保证所有IO都能够正常使用。
    
    Signed-off-by: zla <zhenglongan@163.com>

--- project_code/Project/MDKARM/Objects/initial.d
+++ project_code/Project/MDKARM/Objects/initial.d
@@ -1,7 +1,7 @@
 .\objects\initial.o: ..\..\Applications\User\src\initial.c
 .\objects\initial.o: ..\..\Applications\User\inc\common.h
 .\objects\initial.o: ..\..\Applications\User\inc\typedef.h
-.\objects\initial.o: C:\Keil_v5\ARM\ARMCC\include\stdint.h
+.\objects\initial.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\objects\initial.o: ..\..\Libraries\CMSIS\Include\cmsis_compiler.h
 .\objects\initial.o: ..\..\Libraries\CMSIS\Include\cmsis_armcc.h
 .\objects\initial.o: ..\..\Applications\System\inc\sys_variable.h
